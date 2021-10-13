53c400f7f388a36ed27b7c6449a111e367b209c0
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-13 19:26:51 +0330

    add post and comment model to the admin panel

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post,Comment
 # Register your models here.
+
+class Post_Admin(admin.ModelAdmin):
+    list_display=("author","title","created","count_like")
+    prepopulated_fields={"slug":("title",)}
+class Comment_Admin(admin.ModelAdmin):
+    list_display=("author","post","count_like","count_replies")
+
+admin.site.register(Post,Post_Admin)
+admin.site.register(Comment,Comment_Admin)
