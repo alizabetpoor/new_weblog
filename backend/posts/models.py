1390b922d5af6f603213b1bdc05d67ec8656dd3a
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-13 11:17:01 +0330

    change the structure of like and comment model

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import BLANK_CHOICE_DASH
 # Create your models here.
 
+#model like of post
+class Like(models.Model):
+    users=models.ManyToManyField(User,blank=True)
+
+
+    def count_likes(self):
+        return self.users.count
 
 
 #model post
@@ -12,15 +20,16 @@ class Post(models.Model):
     image=models.ImageField(upload_to=f"{author.username}/blog_images",verbose_name="عکس مطلب")
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
+    likes=models.ForeignKey(Like,on_delete=models.DO_NOTHING)
 
-
-
-class Like(models.Model):
-    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="likes")
-    users=models.ManyToManyField(User)
-
-    def __str__(self) -> str:
-        return f"عنوان پست:{self.post.title}"
 
     def count_likes(self):
-        return self.users.count
+        return self.likes.count
+
+
+class Comment(models.Model):
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name="comments")
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
+    text=models.TextField(verbose_name="متن کامنت")
+    likes=models.ForeignKey(Like,on_delete=models.DO_NOTHING)
+    replies=models.ForeignKey("self",blank=True,null=True,on_delete=models.CASCADE)
