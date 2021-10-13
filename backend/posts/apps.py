6d5696e7902dd05c9b2caacecb758f4b2f059956
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-13 19:25:44 +0330

    add signals to the app

--- backend/posts/apps.py
+++ backend/posts/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'posts'
+    def ready(self):
+        import posts.signals
