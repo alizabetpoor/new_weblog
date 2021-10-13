a646a5021cb0f0d5365840d2ea885411ab62f3ed
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-13 19:25:22 +0330

    change the structure of post models

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.fields import BLANK_CHOICE_DASH
 # Create your models here.
-
-#model like of post
-class Like(models.Model):
-    users=models.ManyToManyField(User,blank=True)
-
-
-    def count_likes(self):
-        return self.users.count
 
 
 #model post
@@ -17,19 +8,37 @@ class Post(models.Model):
     author=models.ForeignKey(User,on_delete=models.CASCADE,related_name="posts")
     title=models.CharField(max_length=70,verbose_name="عنوان")
     slug=models.SlugField(max_length=40,verbose_name="لینک مطلب")
-    image=models.ImageField(upload_to=f"{author.username}/blog_images",verbose_name="عکس مطلب")
+    image=models.ImageField(upload_to="blog_images",verbose_name="عکس مطلب")
+    text=models.TextField(verbose_name="متن پست")
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
-    likes=models.ForeignKey(Like,on_delete=models.DO_NOTHING)
+    likes=models.ManyToManyField(User,blank=True)
 
 
-    def count_likes(self):
-        return self.likes.count
+    def __str__(self) -> str:
+        return f"عنوان مطلب:{self.title}"
+
+
+    def count_like(self):
+        return self.likes.count()
+
+    count_like.short_description="تعداد لایک"
+
 
 
 class Comment(models.Model):
     author=models.ForeignKey(User,on_delete=models.CASCADE,related_name="comments")
     post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
     text=models.TextField(verbose_name="متن کامنت")
-    likes=models.ForeignKey(Like,on_delete=models.DO_NOTHING)
-    replies=models.ForeignKey("self",blank=True,null=True,on_delete=models.CASCADE)
+    likes=models.ManyToManyField(User,blank=True)
+    parent_comment=models.ForeignKey("self",blank=True,null=True,on_delete=models.CASCADE,related_name="replies")
+
+    def count_replies(self):
+        return self.replies.count() 
+
+    count_replies.short_description="تعداد رپلای"
+
+    def count_like(self):
+        return self.likes.count()
+
+    count_like.short_description="تعداد لایک"
