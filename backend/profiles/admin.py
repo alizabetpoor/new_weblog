59fa154eeef0d83cd67f4bcedc91a28eb61ffd51
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-14 16:42:30 +0330

    add profiles to the admin panel

--- backend/profiles/admin.py
+++ backend/profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+
+class Profile_Admin(admin.ModelAdmin):
+    list_display=("user","display_name","birthday","phonenumber",)
+
+
+admin.site.register(Profile,Profile_Admin)
