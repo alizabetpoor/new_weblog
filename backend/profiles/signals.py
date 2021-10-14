3c42ed3aeae19a92f6b67a680ed5ab97ecc12e6d
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-14 17:10:01 +0330

    use custom user model

--- backend/profiles/signals.py
+++ backend/profiles/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
 from .models import Profile
-from django.contrib.auth.models import User
+from .models import User
 from django.db.models.signals import post_save
 
 
