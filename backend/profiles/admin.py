8613111461cf2ee0b07b59e992b38b4ada7b8a62
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-14 17:09:32 +0330

    add custom user model to the admin panel

--- backend/profiles/admin.py
+++ backend/profiles/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Profile
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile,User
 # Register your models here.
 
 class Profile_Admin(admin.ModelAdmin):
     list_display=("user","display_name","birthday","phonenumber",)
 
+
+
+
+UserAdmin.add_fieldsets[0][1]["fields"]=("email",'username',
+                                        'password1', 'password2')
+
+UserAdmin.list_display = ('username', 'email', 'first_name',
+                         'last_name','is_staff')
+
+admin.site.register(User,UserAdmin)
 
 admin.site.register(Profile,Profile_Admin)
