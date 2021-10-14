738d5ebc16a6a64a4ff09b74ccfeaedfddae10f8
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-14 17:10:18 +0330

    create custom user model

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import re
-from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 def validate_phonenumber(phonenumber):
@@ -10,8 +10,16 @@ def validate_phonenumber(phonenumber):
         raise ValidationError(('phone number is not correct'),params={'phonenumber':phonenumber})
 
 
+class User(AbstractUser):
+    email=models.EmailField(unique=True,null=False,verbose_name="ایمیل")
+    REQUIRED_FIELDS = ['email']
+    def __str__(self) -> str:
+        return f"{self.username}"
+
 class Profile(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="profile")
     display_name=models.CharField(max_length=100,null=True,blank=True)
     birthday=models.DateField(null=True,blank=True)
     phonenumber=models.CharField(max_length=13,unique=True,null=True,blank=True,validators=[validate_phonenumber])
+    phone_verify=models.BooleanField(default=False,verbose_name="وریفای ایمیل")
+    email_verify=models.BooleanField(default=False,verbose_name="وریفای شماره")
