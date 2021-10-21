3e6ec8750151c206e91f75644f657cb575d92d7b
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-21 14:49:02 +0330

    change app component structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
-
+import Layout from "./layout/Layout";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App flex flex-col items-center">
+      <Layout>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+        </Switch>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
