9a9427d410dae5a423d3cd93b2fb6855f4da6644
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-10-21 14:50:05 +0330

    add homepage component

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import RandomPosts from "../components/RandomPosts/RandomPosts";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <RandomPosts />
+    </div>
+  );
+};
+
+export default HomePage;
