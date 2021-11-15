a7895f009f5220b40884408a0882c109e79eafe8
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-11-15 19:28:41 +0330

    add user serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework.serializers import ModelSerializer
 from posts.models import Category,Post,Comment
+from profiles.models import User
+
+
+
+class User_Serilizer(ModelSerializer):
+    class Meta:
+        model=User
+        fields=["id","username","first_name","last_name",]
 
 class Post_Serializer(ModelSerializer):
+    author=User_Serilizer(many=False,read_only=True)
     class Meta:
         model=Post
         fields="__all__"
-        read_only_fields = ["author"]
+        #read_only_fields = ["author"]
 
 
 class Comment_Serializer(ModelSerializer):
@@ -18,3 +27,5 @@ class Category_Serializer(ModelSerializer):
     class Meta:
         model=Category
         fields="__all__"
+
+
