3486a250f05afb1e236f9c9cd861d5c857f366c7
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-11-15 19:29:32 +0330

    add another routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,30 @@
 import "./App.css";
 import Layout from "./layout/Layout";
 import { Route, Switch } from "react-router-dom";
+import ScrollToTop from "./utils/ScrollToTop";
 import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import PostPage from "./pages/PostPage";
+import Profile from "./pages/Profile";
+import { ToastProvider } from "react-toast-notifications";
 function App() {
   return (
     <div className="App flex flex-col items-center">
-      <Layout>
-        <Switch>
-          <Route path="/" component={HomePage} exact />
-        </Switch>
-      </Layout>
+      <ToastProvider>
+        <Layout>
+          <ScrollToTop />
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/register" component={RegisterPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/post/:id" component={PostPage} />
+            <Route path="/profile/:username" component={Profile} />
+            <Route path="" component={NotFoundPage} />
+          </Switch>
+        </Layout>
+      </ToastProvider>
     </div>
   );
 }
