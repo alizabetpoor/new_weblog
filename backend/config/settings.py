83a442088ef4b056523f3f70d5ccf5ac965ceeb0
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-11-17 20:32:26 +0330

    update jwt setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -166,8 +166,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
 
