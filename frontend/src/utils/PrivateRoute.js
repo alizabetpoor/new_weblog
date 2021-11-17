0cb8aef72f6d902ad645da41fc619869f846a084
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-11-17 20:34:44 +0330

    add private route

---
+++ frontend/src/utils/PrivateRoute.js
@@ -0,0 +1,9 @@
+import { Route, Redirect } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+const PrivateRoute = ({ children, ...rest }) => {
+  let { user } = useContext(AuthContext);
+  return <Route {...rest}>{!user ? <Redirect to="/" /> : children}</Route>;
+};
+
+export default PrivateRoute;
