50851b75a1a83bce32b87f55eac5a5812895c70c
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-18 08:51:13 +0330

    add profile and bookmark serializer and imagefield to the post serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,21 +1,36 @@
-from rest_framework.serializers import ModelSerializer
-from posts.models import Category,Post,Comment
-from profiles.models import User
+from rest_framework.serializers import ModelSerializer,SerializerMethodField,ImageField
+from posts.models import Category,Post,Comment,Bookmark
+from profiles.models import User,Profile
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 
-
+class Profile_Serializer(ModelSerializer):
+    class Meta:
+        model=Profile
+        fields="__all__"
 class User_Serilizer(ModelSerializer):
+    profile=Profile_Serializer(read_only=True,many=False)
     class Meta:
         model=User
-        fields=["id","username","first_name","last_name",]
+        fields=["id","username","first_name","last_name","profile"]
 
 class Post_Serializer(ModelSerializer):
     author=User_Serilizer(many=False,read_only=True)
+    image=ImageField()
+    time_post_created = SerializerMethodField()
+    def get_time_post_created(self, obj):
+        return naturaltime(obj.created)
     class Meta:
         model=Post
         fields="__all__"
         #read_only_fields = ["author"]
 
+class Bookmark_Serializer(ModelSerializer):
+    user=User_Serilizer(many=False,read_only=True)
+    class Meta:
+        model=Bookmark
+        fields="__all__"
+        depth=1
 
 class Comment_Serializer(ModelSerializer):
     class Meta:
