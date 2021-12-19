0d6b06e6d3cb2ac049b75785054ae6c8785495eb
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-19 09:55:21 +0330

    add api & show content as dangerouslyhtml & category of post

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -1,49 +1,105 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import profilepic from "../../assets/images/profile.webp";
 import { BiBookmark, BiHeart } from "react-icons/bi";
-import postpic from "../../assets/images/post.jpeg";
+import AuthContext from "../../context/AuthContext";
+import { useToasts } from "react-toast-notifications";
+import useAxios from "../../utils/UseAxios";
 const Post = (props) => {
+  const { addToast } = useToasts();
+  const { user } = useContext(AuthContext);
+  const api = useAxios();
+  const [post, setPost] = useState(props.data);
   const [liked, setLiked] = useState(null);
   const [countLike, setCountLike] = useState(null);
   const likeHandler = () => {
-    liked ? setCountLike((prev) => prev - 1) : setCountLike((prev) => prev + 1);
-    setLiked((prev) => !prev);
+    if (user) {
+      if (liked) {
+        let list_liked = [...post.likes];
+        let new_list_like = list_liked.filter((like) => like !== user.user_id);
+        api
+          .patch(`/posts/${post.id}/`, { likes: new_list_like })
+          .then((res) => {
+            console.log(res);
+            setCountLike((prev) => prev - 1);
+          })
+          .catch((err) => console.log(err));
+      } else {
+        api
+          .patch(`/posts/${post.id}/`, { likes: [user.user_id, ...post.likes] })
+          .then((res) => {
+            console.log(res);
+            setCountLike((prev) => prev + 1);
+          })
+          .catch((err) => console.log(err));
+      }
+      setLiked((prev) => !prev);
+    } else {
+      addToast("ابتدا لاگین کنید.", {
+        appearance: "error",
+        autoDismiss: true,
+        autoDismissTimeout: 3000,
+      });
+    }
   };
   useEffect(() => {
-    setLiked(false);
-    setCountLike(props.data.like);
-  }, [props.data.like]);
+    if (user) {
+      if (post.likes.includes(user.user_id)) {
+        setLiked(true);
+      }
+    }
+    setCountLike(post.likes.length);
+  }, []);
+
   return (
     <div className="bg-white p-3 flex flex-col w-full rounded-md leading-10 text-gray-500">
       <div className="flex justify-between">
         <div className="flex items-center">
           <div>
-            <img className="rounded-full w-12 h-12" src={profilepic} alt="" />
+            <img
+              className="rounded-full w-12 h-12"
+              src={post.author.profile.profile_photo || profilepic}
+              alt=""
+            />
           </div>
           <div className="flex flex-col mr-3 items-start">
-            <Link to="/profile/alizabetpoor">
-              <span className="text-gray-700">{props.data.username}</span>
+            <Link to={`/profile/${post.author.username}`}>
+              <span className="text-gray-700">{post.author.username}</span>
             </Link>
-            <span className="text-xs">{props.data.created}</span>
+            <span className="text-xs">{post.time_post_created}</span>
           </div>
         </div>
         <div>
-          <Link to="/barnamenevisi" className="text-gray-700">
-            {props.data.category}
-          </Link>
+          {post.category
+            ? post.category.map((cat, index) => {
+                return (
+                  <Link
+                    key={index}
+                    to="/barnamenevisi"
+                    className="text-gray-700"
+                  >
+                    {cat}
+                  </Link>
+                );
+              })
+            : null}
         </div>
       </div>
       <div className="self-start my-6 font-sahelbold font-extrabold text-xl text-gray-700">
-        <Link to="/post/1">
-          <h1>{props.data.title}</h1>
+        <Link to={`/post/${post.id}`}>
+          <h1>{post.title}</h1>
         </Link>
       </div>
       <div>
-        <img className="w-full h-52" src={postpic} alt="" />
+        <img className="w-full h-52" src={post.image} alt="" />
       </div>
       <div className="self-start">
-        <p>{props.data.description}</p>
+        <p
+          className="text-right"
+          dangerouslySetInnerHTML={{
+            __html: post.text.substring(0, 50) + " ...",
+          }}
+        />
       </div>
       <div className="flex justify-between items-center text-2xl">
         <div className="flex space-x-2 items-center space-x-reverse">
