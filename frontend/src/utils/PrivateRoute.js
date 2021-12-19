4d66d29c34f2433ffb3141001e10418b648683af
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-19 10:00:32 +0330

    redirect user to the login page if the user is not logged in

--- frontend/src/utils/PrivateRoute.js
+++ frontend/src/utils/PrivateRoute.js
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 const PrivateRoute = ({ children, ...rest }) => {
   let { user } = useContext(AuthContext);
-  return <Route {...rest}>{!user ? <Redirect to="/" /> : children}</Route>;
+  return <Route {...rest}>{!user ? <Redirect to="/login" /> : children}</Route>;
 };
 
 export default PrivateRoute;
