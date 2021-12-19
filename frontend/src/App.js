3e67ff1c1107b52dd08e073c6f31d7ec8ab53ff7
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-19 09:51:52 +0330

    add authprovider & private route of newpost

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,22 +8,31 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import PostPage from "./pages/PostPage";
 import Profile from "./pages/Profile";
+import NewPostPage from "./pages/NewPostPage";
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
 import { ToastProvider } from "react-toast-notifications";
 function App() {
   return (
     <div className="App flex flex-col items-center">
       <ToastProvider>
-        <Layout>
-          <ScrollToTop />
-          <Switch>
-            <Route path="/" component={HomePage} exact />
-            <Route path="/register" component={RegisterPage} />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/post/:id" component={PostPage} />
-            <Route path="/profile/:username" component={Profile} />
-            <Route path="" component={NotFoundPage} />
-          </Switch>
-        </Layout>
+        <AuthProvider>
+          <Layout>
+            <ScrollToTop />
+            <Switch>
+              <Route path="/" component={HomePage} exact />
+
+              <Route path="/register" component={RegisterPage} />
+              <Route path="/login" component={LoginPage} />
+              <Route path="/post/:id" component={PostPage} />
+              <Route path="/profile/:username" component={Profile} />
+              <PrivateRoute>
+                <Route path="/newpost" component={NewPostPage} />
+              </PrivateRoute>
+              <Route path="" component={NotFoundPage} />
+            </Switch>
+          </Layout>
+        </AuthProvider>
       </ToastProvider>
     </div>
   );
