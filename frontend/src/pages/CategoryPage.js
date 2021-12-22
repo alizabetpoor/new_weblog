81125ec73e18795d4c964471ba758e3400ac1e12
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-22 14:20:44 +0330

    add category page

---
+++ frontend/src/pages/CategoryPage.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import Posts from "../components/Posts";
+import useAxios from "../utils/UseAxios";
+const CategoryPage = (props) => {
+  const [category, setCategory] = useState(null);
+  const api = useAxios();
+  useEffect(() => {
+    console.log("re render");
+
+    api
+      .get(`/categorys/${props.match.params.category}/`)
+      .then((res) => {
+        setCategory(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [props.match.params.category]);
+  return (
+    <div className="flex flex-col items-center">
+      <h1>دسته : {category?.name}</h1>
+      <div className="w-3/5">
+        <Posts endpoint={`/posts/category/${props.match.params.category}/`} />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
