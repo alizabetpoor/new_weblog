7ea9cd5fcc2b314b0d6d564e282f01122995e7e6
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-22 14:21:44 +0330

    add category & remove slug field

--- frontend/src/pages/NewPostPage.js
+++ frontend/src/pages/NewPostPage.js
@@ -1,19 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { EditorState, convertToRaw } from "draft-js";
 import TextEditor from "../components/TextEditor/TextEditor";
 import useAxios from "../utils/UseAxios";
 import Uploader from "../components/Uploader/Uploader";
 import draftToHtml from "draftjs-to-html";
+import Select from "react-select";
 import { useToasts } from "react-toast-notifications";
 
 const NewPostPage = (props) => {
   const api = useAxios();
+  const [categoryOptions, setCategoryOptions] = useState([]);
   const [formState, setFormState] = useState({
     pic: null,
     title: "",
+    selectedCategory: null,
   });
   const { addToast } = useToasts();
-
+  const categoryHandler = (selectedOption) => {
+    setFormState({ ...formState, selectedCategory: selectedOption });
+  };
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
@@ -29,10 +34,12 @@ const NewPostPage = (props) => {
     console.log(formState);
     let text = draftToHtml(convertToRaw(editorState.getCurrentContent()));
     let photo = formState.pic;
+    let category = formState.selectedCategory.value;
     let data = new FormData();
     data.append("image", photo);
     data.append("title", formState.title);
     data.append("text", text);
+    data.append("category", category);
     api
       .post("/posts/", data)
       .then((res) => {
@@ -53,6 +60,20 @@ const NewPostPage = (props) => {
         });
       });
   };
+  useEffect(() => {
+    api
+      .get("/categorys/")
+      .then((res) => {
+        let categorylist = [];
+        res.data.forEach((category) => {
+          categorylist.push({ value: category.id, label: category.name });
+        });
+        setCategoryOptions(categorylist);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="py-3">
       <div className="flex justify-center">
@@ -66,6 +87,14 @@ const NewPostPage = (props) => {
             name="title"
             id="title"
             placeholder="عنوان پست"
+          />
+          <label htmlFor="category" className="self-start">
+            دسته بندی
+          </label>
+          <Select
+            value={formState.selectedCategory}
+            onChange={categoryHandler}
+            options={categoryOptions}
           />
           <label className="self-start">تصویر پست</label>
           <div className="h-56 w-1/2 self-center">
