c3d18fd282dcba9b64fc4643659150f1bd635b24
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-28 21:36:04 +0330

    change media root & base root because the change of setting file django

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../db.sqlite3',
     }
 }
 
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
     # },
 ]
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / '../media'
 MEDIA_URL = '/media/'
 
 
