beecc6b82a6e5cd62004e4df7b73167764ac8b0f
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-28 21:37:40 +0330

    add props to disable the input

--- frontend/src/components/Inputs/TextInput.js
+++ frontend/src/components/Inputs/TextInput.js
@@ -7,10 +7,13 @@ const TextInput = (props) => {
       <input
         {...props.formik.getFieldProps(props.name)}
         name={props.name}
-        className="border-2 border-gray-400 p-1 rounded-md"
+        className={`${
+          props.disabled && "opacity-60"
+        } mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md`}
         placeholder={props.placeholder}
         type={props.type ? props.type : "text"}
         id={props.name}
+        disabled={props.disabled}
       />
       {props.formik.errors[props.name] && props.formik.touched[props.name] && (
         <div className="text-red-500 self-start">
