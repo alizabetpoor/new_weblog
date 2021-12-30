fc1a86028a0d1e75782e8c4a68b6adf88fa267e7
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-30 15:31:36 +0330

    import themesberg

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "@themesberg/flowbite";
+
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
