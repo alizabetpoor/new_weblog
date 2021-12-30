5d2c46f258ee7bd13d421b62abe18ee5b00dfad2
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-30 15:33:44 +0330

    showing register element just for anonymous user

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,13 @@
 import Posts from "../components/Posts";
 import { BiChevronLeft } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 import enamadphoto from "../assets/images/enamad.png";
 import RandomPosts from "../components/RandomPosts/RandomPosts";
+import AuthContext from "../context/AuthContext";
 import PopularPosts from "../components/PopularPosts/PopularPosts";
 const HomePage = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="homepage w-full space-y-8 flex flex-col items-center">
       <RandomPosts />
@@ -13,22 +16,25 @@ const HomePage = () => {
           <Posts endpoint="/posts/" />
         </main>
         <aside className="w-4/12 mt-3 lg:block hidden">
-          <div className="text-right w-full">
-            <h2 className="font-sahelbold font-bold text-base">
-              به وبلاگ ما خوش آمدید
-            </h2>
-            <p className="text-gray-500">
-              زمان آن رسیده که شیوه‌ی نوشتن و خواندن مطالبتان را تغییر دهید. اگر
-              برای آغاز دوران جدید آماده هستید به ویرگول خوش آمدید.
-            </p>
-            <Link
-              className="flex px-5 py-3 w-2/3 bg-blue-600 hover:bg-blue-700 transition-all mt-4 text-white justify-between items-center rounded-3xl"
-              to="/register"
-            >
-              <span className="text-sm">میخواهم ثبت نام کنم</span>
-              <BiChevronLeft className="text-xl" />
-            </Link>
-          </div>
+          {!user && (
+            <div className="text-right w-full">
+              <h2 className="font-sahelbold font-bold text-base">
+                به وبلاگ ما خوش آمدید
+              </h2>
+              <p className="text-gray-500">
+                زمان آن رسیده که شیوه‌ی نوشتن و خواندن مطالبتان را تغییر دهید.
+                اگر برای آغاز دوران جدید آماده هستید به ویرگول خوش آمدید.
+              </p>
+              <Link
+                className="flex px-5 py-3 w-2/3 bg-blue-600 hover:bg-blue-700 transition-all mt-4 text-white justify-between items-center rounded-3xl"
+                to="/register"
+              >
+                <span className="text-sm">میخواهم ثبت نام کنم</span>
+                <BiChevronLeft className="text-xl" />
+              </Link>
+            </div>
+          )}
+
           <div className="sticky top-14">
             <PopularPosts />
             <div className="mt-10 space-y-4">
