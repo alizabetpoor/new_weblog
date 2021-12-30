0b18eed8cf0626f6aef07866472a16c85662ed03
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-30 15:31:11 +0330

    add themesberg plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,5 +15,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@themesberg/flowbite/plugin"),
+  ],
 };
