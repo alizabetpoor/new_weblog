dd431a3718de8a0460be3184260221f8259081c4
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-12-30 15:32:36 +0330

    add inputclass for classes of input

--- frontend/src/components/Inputs/TextInput.js
+++ frontend/src/components/Inputs/TextInput.js
@@ -1,4 +1,11 @@
 const TextInput = (props) => {
+  let inputClass = `${
+    props.checkvalid && !Object.keys(props.formik.errors).includes(props.name)
+      ? "bg-green-50 border border-green-500 text-green-900 placeholder-green-700 focus:ring-green-500 focus:border-green-500"
+      : null
+  } ${
+    props.disabled && "opacity-60"
+  } mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md`;
   return (
     <div className="flex flex-col items-stretch">
       <label className="self-start" htmlFor={props.name}>
@@ -7,9 +14,7 @@ const TextInput = (props) => {
       <input
         {...props.formik.getFieldProps(props.name)}
         name={props.name}
-        className={`${
-          props.disabled && "opacity-60"
-        } mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md`}
+        className={inputClass}
         placeholder={props.placeholder}
         type={props.type ? props.type : "text"}
         id={props.name}
