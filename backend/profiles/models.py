4341a34e3e0bf606f74677888d232316547c46c9
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-01-05 14:18:40 +0330

    add userfollowing model

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -16,6 +16,16 @@ class User(AbstractUser):
     def __str__(self) -> str:
         return f"{self.username}"
 
+
+class UserFollowing(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="following")
+    following_user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="followers")
+    created=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ('user', 'following_user',)
+
+    def __str__(self):
+        return f"{self.user.username}({self.user.id}) follow the {self.following_user.username}({self.following_user.id})"
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
     display_name=models.CharField(max_length=100,null=True,blank=True)
@@ -24,3 +34,10 @@ class Profile(models.Model):
     phonenumber=models.CharField(max_length=13,unique=True,null=True,blank=True,validators=[validate_phonenumber])
     phone_verify=models.BooleanField(default=False,verbose_name="وریفای ایمیل")
     email_verify=models.BooleanField(default=False,verbose_name="وریفای شماره")
+
+    def get_following(self):
+        return self.user.following.values_list('id', flat=True)
+
+    def get_followers(self):
+        return self.user.followers.values_list('id', flat=True)
+    
