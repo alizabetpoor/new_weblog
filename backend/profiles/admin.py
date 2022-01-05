b5fa8e326e36955689a478f0525c1253bf1f6408
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-01-05 14:18:26 +0330

    add following model to django admin

--- backend/profiles/admin.py
+++ backend/profiles/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Profile,User
+from .models import Profile,User,UserFollowing
 # Register your models here.
 
 class Profile_Admin(admin.ModelAdmin):
     list_display=("user","display_name","birthday","phonenumber",)
 
+
+class Following_Admin(admin.ModelAdmin):
+    list_display=("user","following_user","created")
 
 
 
@@ -15,6 +18,11 @@ UserAdmin.add_fieldsets[0][1]["fields"]=("email",'username',
 UserAdmin.list_display = ('username', 'email', 'first_name',
                          'last_name','is_staff')
 
+
+
+
 admin.site.register(User,UserAdmin)
 
 admin.site.register(Profile,Profile_Admin)
+
+admin.site.register(UserFollowing,Following_Admin)
