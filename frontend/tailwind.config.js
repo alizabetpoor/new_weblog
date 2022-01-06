4502c59c303a7e9057827e95aab1ca2ce20e9c41
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-01-06 20:34:03 +0330

    add daisyui settings

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", //false, // or 'media' or 'class'
+  daisyui: {
+    styled: true,
+    themes: false,
+    rtl: true,
+  },
   theme: {
     extend: {
       fontFamily: {
@@ -15,8 +20,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@themesberg/flowbite/plugin"),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("daisyui")],
 };
