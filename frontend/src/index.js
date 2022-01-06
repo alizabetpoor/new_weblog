2b3304305c6f26b66476cd1a9ff7dc549a6f5532
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-01-06 20:34:26 +0330

    delete themesberg

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "@themesberg/flowbite";
 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
