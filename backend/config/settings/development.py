feeeb91234094cf751bf906a54b22f16223aa210
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-01-24 12:41:36 +0330

    update development setting

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -1,4 +1,5 @@
 from config.settings.base import *
+from decouple import config
 
 
 #override base settings here
@@ -6,4 +7,14 @@ from config.settings.base import *
 DEBUG=True
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('db_name'),
+        'USER': config('db_user'),
+        'PASSWORD':config('db_pass'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
